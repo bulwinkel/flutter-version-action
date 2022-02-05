8d4b0beb350461924e723c4cc6d4f0fb573bde16
Kelvin Bulwinkel <kelvin@bulwinkel.com>
2022-02-05 18:32:47 +1000

    Add more logging

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const { updateVersionInPubspecs } = require("./flutter-versioning");
+const fs = require("fs");
 
 // most @actions toolkit packages have async methods
 async function run() {
@@ -8,6 +9,12 @@ async function run() {
     core.info(`pubspec_path: ${path}`);
 
     const { newVersion, newBuild } = updateVersionInPubspecs({ path });
+    core.info(`Version successfully updated in ${path}`);
+    core.info("----");
+    core.info(`new_version: ${newVersion}`);
+    core.info(`new_build: ${newBuild}`);
+    core.info("----");
+    core.info(fs.readFileSync(path, "utf8").toString());
 
     core.setOutput("new_version", newVersion);
     core.setOutput("new_build", newBuild);
